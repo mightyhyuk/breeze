77f67b5d9ff49e3a14a4c7f8d7e01dbf100afd9c
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-01-11 17:24:59 +0900

    Create components: Song and Player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Song from "./components/Song";
+import Player from "./components/Player";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }

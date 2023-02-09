ef16bb4fdab54ed05781e8c63e4f30be8663476f
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2023-02-09 13:58:33 +0900

    Update music data

--- src/data/songs.js
+++ src/data/songs.js
@@ -2,47 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 
 const songs = [
   {
-    title: "Oasis",
-    coverImg:
-      "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
-    artist: "Makzo",
-    audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
-    id: uuidv4(),
-    isActive: true,
-  },
-  {
-    title: "Colors Fade",
-    coverImg:
-      "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
-    artist: "Sleepy Fish",
-    audio: "https://mp3.chillhop.com/serve.php/?mp3=10023",
-    id: uuidv4(),
-    isActive: false,
-  },
-  {
-    title: "Droplets",
-    coverImg:
-      "https://chillhop.com/wp-content/uploads/2023/01/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
-    artist: "Philanthrope",
-    audio: "https://mp3.chillhop.com/serve.php/?mp3=10257",
-    id: uuidv4(),
-    isActive: false,
-  },
-  {
     title: "Ultraviolet",
     coverImg:
       "https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg",
     artist: "Smile High, Teddy Roxpin",
     audio: "https://mp3.chillhop.com/serve.php/?mp3=23345",
     id: uuidv4(),
-    isActive: false,
+    isActive: true,
   },
   {
-    title: "Keep Going",
+    title: "I Main Samus Now",
     coverImg:
-      "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-    artist: "Swørn",
-    audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      "https://chillhop.com/wp-content/uploads/2021/09/2ce75252f5419a45d76bb93424ac1eae3e688b17-1024x1024.jpg",
+    artist: "Sleepy Fish",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=24822",
     id: uuidv4(),
     isActive: false,
   },
@@ -56,11 +29,11 @@ const songs = [
     isActive: false,
   },
   {
-    title: "Reflection",
+    title: "Vintage",
     coverImg:
-      "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-    artist: "Swørn",
-    audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg",
+    artist: "Evil Needle",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=17088",
     id: uuidv4(),
     isActive: false,
   },
@@ -79,6 +52,33 @@ const songs = [
       "https://chillhop.com/wp-content/uploads/2021/09/a2d710149876c8e0c9fb097840b2438fbc21bd37-1024x1024.jpg",
     artist: "Aviino",
     audio: "https://mp3.chillhop.com/serve.php/?mp3=23065",
+    id: uuidv4(),
+    isActive: false,
+  },
+  {
+    title: "Oasis",
+    coverImg:
+      "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+    artist: "Makzo",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
+    id: uuidv4(),
+    isActive: false,
+  },
+  {
+    title: "Colors Fade",
+    coverImg:
+      "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+    artist: "Sleepy Fish",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=10023",
+    id: uuidv4(),
+    isActive: false,
+  },
+  {
+    title: "Droplets",
+    coverImg:
+      "https://chillhop.com/wp-content/uploads/2023/01/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+    artist: "Philanthrope",
+    audio: "https://mp3.chillhop.com/serve.php/?mp3=10257",
     id: uuidv4(),
     isActive: false,
   },
